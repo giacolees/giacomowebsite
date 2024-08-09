ed16a0e1b246278e87c1d9295ffa0b0f9b96181e
giacolees <giacomolisita01@gmail.com>
2024-08-09 16:39:45 +0200

    Fixed About

--- GiacomoWebsite/src/App.js
+++ GiacomoWebsite/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import React, { useState, useEffect} from 'react';
 import styled from 'styled-components';
 import Header from './components/Header';
